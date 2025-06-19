c4068f04beacbf2d4cd2b311b4faae779e390a5d
nikoanderianperatama12345 <nikopamanukan123@gmail.com>
2025-06-19 17:37:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,21 @@ window.onload = () => {
   const videoEl = document.getElementById("videoPlayer");
   const nextBtn = document.getElementById("nextBtn");
 
-  const alreadyPlayed = localStorage.getItem("videoPlayed") === "yes";
+  fetch("video1.mp4")
+    .then(res => res.blob())
+    .then(blob => {
+      const blobUrl = URL.createObjectURL(blob);
+      videoEl.src = blobUrl;
 
-  if (alreadyPlayed) {
-    videoEl.pause();
-    nextBtn.classList.remove("hidden");
-  } else {
-    fetch("video1.mp4")
-      .then(res => res.blob())
-      .then(blob => {
-        const blobUrl = URL.createObjectURL(blob);
-        videoEl.src = blobUrl;
+      videoEl.onplaying = () => {
+        setTimeout(() => {
+          videoEl.pause();
+          nextBtn.classList.remove("hidden");
+        }, 2000); // 2000 ms = 2 detik
+      };
 
-        videoEl.onplaying = () => {
-          setTimeout(() => {
-            videoEl.pause();
-            nextBtn.classList.remove("hidden");
-            localStorage.setItem("videoPlayed", "yes");
-          }, 3000); // Ganti ke 5000 kalau mau 5 detik
-        };
-
-        videoEl.play().catch(() => {});
-      });
-  }
+      videoEl.play().catch(() => {});
+    });
 
   nextBtn.addEventListener("click", () => {
     window.location.href = "login.html";
